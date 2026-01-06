f98313f5c51f3dd575fa5b26f174503f3887fb6f
viranika97 <i9175834858@gmail.com>
2026-01-06 22:53:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,8 +33,8 @@ FIREBASE_COLLECTION = "users"
 FIREBASE_USER_DOC = "veronika"
 
 # Расписание (московское время UTC+3)
-DAILY_REPORT_TIME = "22:34"  # Ежедневный отчёт
-SHAME_CHECK_TIME = "22:40"   # Проверка на лоханье
+DAILY_REPORT_TIME = "22:54"  # Ежедневный отчёт
+SHAME_CHECK_TIME = "22:56"   # Проверка на лоханье
 WEEKLY_SUMMARY_TIME = "20:00"  # Еженедельные итоги (воскресенье)
 
 # Дата старта работы бота (None = работает сразу)
